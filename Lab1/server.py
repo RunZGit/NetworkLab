2986c653b56ed2e7c16b7d0d6ca7e021ca8a11ac
Runzhi Yang <rollink.yang@gmail.com>
2017-09-15 22:09:15 -0400

    finished basic get method

--- Lab1/server.py
+++ Lab1/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python           # This is client.py file
-import socket, sys, time
+import socket, sys, time, os.path
 
 DEFAULT_BUFFER_SIZE = 1024
 DEFAULT_HOST = '127.0.0.1'
@@ -18,66 +18,68 @@ def main(port):
 			(cSocket, address) = serversocket.accept()
 			# ct = client_thread(cSocket)
 			# ct.run()
-
-			print("Client is ", address)
-
-			parseRequest(cSocket)
-			# dummyRespond(cSocket)
-
-			# requestType, requestBody = parseRequest(cSocket)
-
-			# if requestType == 'GET':
-			# 	handleGetRequest(requestBody)
-			# elif requestType == 'HEAD':
-			# 	handleHeadRequest(requestBody)
-			# else:
-			# 	handleBadRequest()
-
+			requestType, filePath = parseRequest(cSocket)
+			handleRequest(cSocket, requestType, filePath)
 			cSocket.close()
-			print "finsihed process"
 		except KeyboardInterrupt:
-			print 'Bye bye!\n'
+			print '\rBye bye!\n'
+			serversocket.close()
 			sys.exit()
 
-def dummyRespond(cSocket):
-	f = open('./HTTP_Reply_messages/200.html', "r")
-	l = f.read(1024)
-	while l:
-		cSocket.send(l)
-		l = f.read(1024)
-	f.close()
-	print "done sending file!"
-	pass
+def handleRequest(cSocket, requestType, filePath):
+	if requestType == 'GET':
+		handleGetRequest(cSocket,filePath)
+	# elif requestType == 'HEAD':
+	# 	handleHeadRequest(requestBody)
+	# else:
+	# 	handleBadRequest(requestBody)
 
 def parseRequest(cSocket):
-	temp = cSocket.recv(DEFAULT_BUFFER_SIZE)
-	# raw_request = "temp[:]\n adas"
-	while temp:
-		print temp
-		print "I am before read"
-		dummyRespond(cSocket)
-		# time.sleep(1)
-		cSocket.settimeout(5.0) 
-		temp = cSocket.recv(DEFAULT_BUFFER_SIZE)
-		print "I am after read"
-		temp = None
-		# raw_request+=temp[:]
-	# print raw_request.split('\n')[0]
-	print "I am here"
-	# dummyRespond(cSocket)
-	pass
+	request = cSocket.recv(DEFAULT_BUFFER_SIZE)
+	request = request.split()
+	requestType = request[0]
+	filePath = request[1]
+	return (requestType, filePath)
 
-def handleGetRequest():
-	pass
+def handleGetRequest(cSocket, path):
+	if path == "/":
+		path = "/index.html"
+
+	path = DEFAULT_FILE_LOCATIONS+path
+	print "Looking for file: " + path
+	content = loadFile(path)
+	if not content:
+		print "404 Not Found"
+		http_response = "HTTP/1.0 404 Not Found \r\n\n"
+	else:
+		print "200 OK"
+		http_response = "HTTP/1.0 200 OK \r\n"
+
+		if path.endswith(".html"):
+			http_response+= "Content-Type: text/html\r\n"
+		elif path.endswith(".jpg"):
+			http_response+= "Content-Type: image/gif\r\n"
+		http_response += 'Last-Modified: ' + time.ctime(os.path.getmtime(path)) + '\r\n'
+		http_response += 'Content-Length: ' + str(len(content)) +'\r\n\n'
+		http_response+=content
+
+	cSocket.sendall(http_response)
 
 def handleHeadRequest():
 	pass
 
-def handleBadRequest():
+def handleBadRequest(cSocket):
+
 	pass
 
+def loadFile(filePath):
+	if os.path.exists(filePath):
+		with open(filePath, 'r') as f:
+			content = f.read()
+		return content
+	return False
+
 if __name__ =='__main__':
-	# main(sys.argv[1:])
 	args = sys.argv[1:]
 	if not len(args) == 1 or not args[0].isdigit():
 		print 'Command not supported. \nExample: python server.py 1234\n'
